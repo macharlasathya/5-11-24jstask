0a728c46da8a1a5670ddd7d17b2f94c671ffd516
macharlasathya <sathishmacharla5995@gmail.com>
2024-11-07 12:15:35 +0530

    adding the task files into repository

---
+++ task1.js
@@ -0,0 +1,9 @@
+let M=document.getElementById("x");
+let W=document.getElementById("y");
+W.ondblclick=function ()
+{
+    M.textContent="Welcome to Module three DOM";
+    M.style.backgroundColor="red";
+    M.style.fontSize="60px";
+    M.style.borderRadius="50%";
+}
---
+++ task2.js
@@ -0,0 +1,9 @@
+let data = document.getElementById("D");
+let Images = document.getElementById("C");
+Images.onclick = function() {
+  data.innerHTML = `<img style="border-radius: 30px; width: 100px; height: 100px;" src="https://cdn.pixabay.com/photo/2024/05/24/02/09/krishna-8784102_1280.jpg">
+    <img style="border-radius: 100px; width: 100px; height: 100px;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8pw-zOHnw5FAouOkeltybbtz0DRWNhfSPFw&s">
+    <img style="border-radius: 50%; width: 100px; height: 100px;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTndCYhtaJAiGucS6pwdp3ZCWyDFoodobXshw&s">`;
+}
+
+
